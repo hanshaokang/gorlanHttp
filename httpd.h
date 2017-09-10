7d232d91c059461c9bfae1b21f7c31ab006c629b
alanxgorlan <alanxgorlan@163.com>
2017-09-10 20:24:06 +0800

    echo_error

--- httpd.h
+++ httpd.h
@@ -13,8 +13,14 @@
 #include <string.h>
 #include <sys/sendfile.h>
 #define SIZE 1024
+#define SUCCESS 0
+#define NOTICE  1
+#define WARNING 2
+#define ERROR   3
+#define FATAL   4
 void echo_www(int fd,char *path,int size);
 static void clear_fdhead(int fd);
+static void request_404(int sock);
 static void echo_client(int nums);
 int  startup( const char *ip,int port);
 static int get_line(int fd,char buf[],int len);
